145dc39a7c8849acefa46eb66814d8da5f3193f7
Valentyn Berekelia <yamato.watanabu@gmail.com>
2025-11-26 13:14:39 +0200

    add register,login,logout

--- ContentManagementSystem/Controllers/AccountController.cs
+++ ContentManagementSystem/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Packaging;
 using System.Security.Claims;
 
 namespace ContentManagementSystem.Controllers
@@ -33,10 +34,35 @@ namespace ContentManagementSystem.Controllers
                 }
                 _context.Users.Add(model);
                 _context.SaveChangesAsync();
+
+
                 await Authenticate(model.Email);
                 return RedirectToAction("Index", "Home");
             }
             return View(model);
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string email,string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+            if(user != null)
+            {
+                await Authenticate(email);
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Login isn't valid");
+            return View();
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
         }
         private async Task Authenticate(string userName)
         {
